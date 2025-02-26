94c0bdc990a56bcd5657def0f070a9e2041da788
Nguyen Kim Dien <trietnk01@gmail.com>
2025-02-26 10:59:41 +0700

    Add new code

--- stack-be/src/app.js
+++ stack-be/src/app.js
@@ -1,28 +1,16 @@
-var http = require("http");
-require("dotenv").config();
-var express = require("express");
-var cors = require("cors");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var bodyParser = require("body-parser");
-require("module-alias/register");
-const { Server } = require("socket.io");
-var port = process.env.NODE_ENV === "development" ? process.env.PORT_DEV : process.env.PORT_PRODUCTION;
-global.__IMAGES = path.join(__dirname, "public/images");
-global.__DOCUMENTS = path.join(__dirname, "public/documents");
-var app = express();
-var serverHttp = http.createServer(app);
-const io = new Server(serverHttp, {
-	cors: { origin: "*" }
-});
-app.use("/", express.static("src/public"));
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(cors());
-var apiRoute = require("@routes/api")(io);
-app.use("/api", apiRoute);
-app.set("port", port);
-serverHttp.listen(port);
+const checkedColor=(val)=>{	
+	let checked=true;
+    const len = val.length;    
+    for (let i = 0; i < len / 2; i++) {        
+        if (val[i] !== val[len - 1 - i]) {
+            checked=false;
+        }
+    }
+    return checked;
+}
+const result= checkedColor("madam");
+if(result===true){
+	console.log("It is palindrome");
+}else{
+	console.log("It is not palindrome");
+}
